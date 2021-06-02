d8765d01d0214b29b43685fad42eee16d3f8d99b
yuqiufeng <15088601208@163.com>
2021-06-02 22:37:20 +0800

    feat: dynamic message

---
+++ tuya-messaging/src/main/java/com/tuya/connector/open/messaging/autoconfig/EnableDynamicMessaging.java
@@ -0,0 +1,14 @@
+package com.tuya.connector.open.messaging.autoconfig;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import(DynamicMessageManager.class)
+public @interface EnableDynamicMessaging {
+}
