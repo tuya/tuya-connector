55a03011a85d87b0b115d6791b761ccb5050326e
q1763868326 <1763868326@qq.com>
2021-08-10 11:42:57 +0800

    hotfix

--- tuya-api/src/test/java/com/tuya/connector/open/api/SecurityInfo.java
+++ tuya-api/src/test/java/com/tuya/connector/open/api/SecurityInfo.java
@@ -5,6 +5,6 @@ package com.tuya.connector.open.api;
  * @since 2021/3/26 11:19 上午
  */
 public class SecurityInfo {
-    public static final String AK = "u9rynj7taekj44qkq0ce";
-    public static final String SK = "2207ea36344b4fcd82d24f4497251f11";
+    public static final String AK = "*********";
+    public static final String SK = "*********";
 }
