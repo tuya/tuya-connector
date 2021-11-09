de2bbf6563a2412359ff3003a9eb6e0505801772
zheye <zheye.zhang@tuya.com>
2021-11-09 15:08:36 +0800

    feat: ak/sk supports multi-thread

--- tuya-spring-boot-starter-sample/src/test/java/com/tuya/open/spring/boot/sample/TuyaSpringBootStarterSampleApplicationTests.java
+++ tuya-spring-boot-starter-sample/src/test/java/com/tuya/open/spring/boot/sample/TuyaSpringBootStarterSampleApplicationTests.java
@@ -1,18 +1,23 @@
 package com.tuya.open.spring.boot.sample;
 
+import com.alibaba.ttl.threadpool.TtlExecutors;
 import com.tuya.connector.api.config.Configuration;
 import com.tuya.connector.open.messaging.autoconfig.MessageProperties;
 import com.tuya.connector.spring.boot.autoconfigure.ConnectorProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 
+import java.util.concurrent.*;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@Slf4j
+@SpringBootTest(classes = {TuyaSpringBootStarterSampleApplication.class})
 class TuyaSpringBootStarterSampleApplicationTests {
 
     @Autowired
@@ -35,6 +40,46 @@ class TuyaSpringBootStarterSampleApplicationTests {
         System.out.println(messageProperties.getUrl());
         assertNotNull(bean.getApi().getContextManager());
         assertTrue(auto);
+    }
+
+    public ExecutorService executor = Executors.newFixedThreadPool(1);
+
+    @Test
+    public void threadTest() throws InterruptedException {
+        executor = TtlExecutors.getTtlExecutorService(executor);
+
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        Thread t1 = new Thread(() ->{
+            configuration.getApiDataSource().setAk("1");
+            configuration.getApiDataSource().setSk("1");
+            log.info("t1:{} ak:{}, sk:{}", Thread.currentThread(), configuration.getApiDataSource().getAk(), configuration.getApiDataSource().getSk());
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            executor.execute(()->
+                    log.info("t1 child:{} ak:{}, sk:{}", Thread.currentThread(), configuration.getApiDataSource().getAk(), configuration.getApiDataSource().getSk()));
+            countDownLatch.countDown();
+        });
+
+        Thread t2 = new Thread(() ->{
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            configuration.getApiDataSource().setAk("2");
+            configuration.getApiDataSource().setSk("2");
+            log.info("t2:{} ak:{}, sk{}",Thread.currentThread(), configuration.getApiDataSource().getAk(), configuration.getApiDataSource().getSk());
+            executor.execute(() ->
+                    log.info("t2 child:{} ak:{}, sk:{}", Thread.currentThread(), configuration.getApiDataSource().getAk(), configuration.getApiDataSource().getSk()));
+            countDownLatch.countDown();
+        });
+
+        t1.start();
+        t2.start();
+        countDownLatch.await();
     }
 
 }
