4682949f0737146ce1995b87c6612f6d7d7c9791
Mario <mario.yang@tuya.com>
2022-01-20 16:38:55 +0800

    组织code换成id

--- tuya-ability/src/main/java/com/tuya/connector/open/ability/acl/shield/connector/ShieldOrganizationUserConnector.java
+++ tuya-ability/src/main/java/com/tuya/connector/open/ability/acl/shield/connector/ShieldOrganizationUserConnector.java
@@ -16,16 +16,16 @@ import com.tuya.connector.open.ability.common.Page;
  */
 public interface ShieldOrganizationUserConnector {
 
-   @POST("/v1.0/iot-02/organizations/{organization_code}/users")
-   Boolean batchAddUsers(@Path("organization_code") String orgCode,
+   @POST("/v1.0/iot-02/organizations/{organization_id}/users")
+   Boolean batchAddUsers(@Path("organization_id") String orgId,
                          @Body ShieldOrgAddUsersRequest req);
 
-   @DELETE("/v1.0/iot-02/organizations/{organization_code}/users")
-   Boolean batchDeleteUsers(@Path("organization_code") String orgCode,
+   @DELETE("/v1.0/iot-02/organizations/{organization_id}/users")
+   Boolean batchDeleteUsers(@Path("organization_id") String orgCode,
                             @Query("uids") String uids);
 
-   @GET("/v1.0/iot-02/organizations/{organization_code}/users")
-   Page<ShieldOrgUserInfoResponse> pageUsers(@Path("organization_code") String orgCode,
+   @GET("/v1.0/iot-02/organizations/{organization_id}/users")
+   Page<ShieldOrgUserInfoResponse> pageUsers(@Path("organization_id") String orgCode,
                                              @Query("uid") String uid,
                                              @Query("page_no") Integer pageNo,
                                              @Query("page_size") Integer pageSize);
