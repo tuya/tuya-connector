1867d59ea6e2bc4ce08d99d5796eb80451029fe6
Mario <mario.yang@tuya.com>
2022-01-25 16:01:12 +0800

    upgrade tuya-connector version to 1.1.5-snapshot

--- tuya-ability/src/main/java/com/tuya/connector/open/ability/acl/shield/model/request/ShieldUserAddRoleRequest.java
+++ tuya-ability/src/main/java/com/tuya/connector/open/ability/acl/shield/model/request/ShieldUserAddRoleRequest.java
@@ -2,6 +2,8 @@ package com.tuya.connector.open.ability.acl.shield.model.request;
 
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * @author Mario
  * @since 2022/1/10 4:13 PM
@@ -9,5 +11,5 @@ import lombok.Data;
 @Data
 public class ShieldUserAddRoleRequest {
 
-   private String roleCode;
+   private List<String> roleCodes;
 }
