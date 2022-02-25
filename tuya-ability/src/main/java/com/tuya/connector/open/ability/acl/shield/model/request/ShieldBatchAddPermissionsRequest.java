44bfad49c9ba5719547901568a5840b0346e4bf6
zheye <zheye.zhang@tuya.com>
2022-02-25 23:19:18 +0800

    chore: 1.1.5 RC

--- tuya-ability/src/main/java/com/tuya/connector/open/ability/acl/shield/model/request/ShieldBatchAddPermissionsRequest.java
+++ tuya-ability/src/main/java/com/tuya/connector/open/ability/acl/shield/model/request/ShieldBatchAddPermissionsRequest.java
@@ -1,5 +1,6 @@
 package com.tuya.connector.open.ability.acl.shield.model.request;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 import java.util.List;
@@ -11,5 +12,6 @@ import java.util.List;
 @Data
 public class ShieldBatchAddPermissionsRequest {
 
+   @SerializedName("permissions")
    private List<ShieldAddPermissionRequest> permissions;
 }
