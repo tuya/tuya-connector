b80066f9721d5ff82e1f0b76f31e75bf0d12f03b
jialuo <jialuo.zhao@tuya.com>
2022-05-20 12:04:32 +0800

    [feat]rename

--- tuya-ability/src/main/java/com/tuya/connector/open/ability/acl/shield/connector/ShieldRoleConnector.java
+++ tuya-ability/src/main/java/com/tuya/connector/open/ability/acl/shield/connector/ShieldRoleConnector.java
@@ -36,7 +36,7 @@ public interface ShieldRoleConnector {
                                      @Query("gmt_modified_asc") Boolean gmtModifiedAsc);
 
    @GET("/v1.0/iot-02/roles")
-   Page<ShieldRoleInfoResponse> pageV2(@Query("role_codes") String roleCodes,
+   Page<ShieldRoleInfoResponse> pageList(@Query("role_codes") String roleCodes,
                                      @Query("role_name") String roleName,
                                      @Query("dimension_no") String dimensionNo,
                                      @Query("page_no") Integer pageNo,
