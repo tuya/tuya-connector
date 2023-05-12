56b2da676459a10009b940f9f2f78b6bf2d9c656
mariogneto <mariogneto@gmail.com>
2023-05-12 18:37:52 -0300

    password-free/open-door

--- tuya-ability/src/main/java/com/tuya/connector/open/ability/device/connector/DeviceConnector.java
+++ tuya-ability/src/main/java/com/tuya/connector/open/ability/device/connector/DeviceConnector.java
@@ -3,10 +3,7 @@ package com.tuya.connector.open.ability.device.connector;
 import com.tuya.connector.api.annotations.*;
 import com.tuya.connector.api.model.Result;
 import com.tuya.connector.open.ability.common.AbilityPage;
-import com.tuya.connector.open.ability.device.model.request.DeviceCommandRequest;
-import com.tuya.connector.open.ability.device.model.request.DeviceModifyRequest;
-import com.tuya.connector.open.ability.device.model.request.DoorLockTempPasswordRequest;
-import com.tuya.connector.open.ability.device.model.request.DoorLockTempPasswordResponse;
+import com.tuya.connector.open.ability.device.model.request.*;
 import com.tuya.connector.open.ability.device.model.response.*;
 
 import java.util.List;
@@ -159,8 +156,8 @@ public interface DeviceConnector {
     @DELETE("/v1.0/devices/{device_id}/door-lock/temp-passwords/{password_id}")
     Result deleteTempPassword(@Path("device_id") String deviceId, @Path("password_id") String password_id);
 
-    @POST("/v1.0/devices/{device_id}/door-lock/open-door")
-    Result openDoor(@Path("device_id") String deviceId);
+    @POST("/v1.0/devices/{device_id}/door-lock/password-free/open-door")
+    Result openDoor(@Path("device_id") String deviceId, @Body DoorLockFreePasswordOpenDoorRequest doorLockFreePasswordOpenDoorRequest);
 
     @PUT("/v1.0/devices/{device_id}/door-lock/temp-passwords/{password_id}/modify-password")
     Result modifyTempPassword(@Path("device_id") String deviceId, @Path("password_id") String password_id,
---
+++ tuya-ability/src/main/java/com/tuya/connector/open/ability/device/model/request/DoorLockFreePasswordOpenDoorRequest.java
@@ -0,0 +1,8 @@
+package com.tuya.connector.open.ability.device.model.request;
+
+import lombok.Data;
+
+@Data
+public class DoorLockFreePasswordOpenDoorRequest {
+    private String ticket_id;
+}
